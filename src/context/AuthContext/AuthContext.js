aee62851e84a90f1bed41415ba1a095b879b5a39
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2022-02-10 20:13:43 +0100

    remove if in useffect

--- src/context/AuthContext/AuthContext.js
+++ src/context/AuthContext/AuthContext.js
@@ -73,15 +73,13 @@ function AuthProvider({ children }) {
 
   const handleIsLoggedIn = async () => {
     try {
-      if (!auth.user) {
-        // check if there is a user in the session of the server
-        const { data } = await isLoggedIn();
-        console.log("user", data);
-        // update the state with the user
-        setAuth({ user: data });
-        // save the user in browser session memory
-        saveSessionUser(data);
-      }
+      // check if there is a user in the session of the server
+      const { data } = await isLoggedIn();
+      console.log("user", data);
+      // update the state with the user
+      setAuth({ user: data });
+      // save the user in browser session memory
+      saveSessionUser(data);
     } catch (err) {
       // if there is an error then update the state with no user
       setAuth({ user: null });
